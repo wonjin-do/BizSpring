bee8753d163863bc187ac556ac1872fa397814b5
wonjin-do <54175950+wonjin-do@users.noreply.github.com>
2020-03-05 17:14:43 +0900

    javascript 정규식 추가
    email, URL

---
+++ php실습/reg/regEx.js
@@ -0,0 +1,15 @@
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validateURL(URL){
+    var expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
+    if (URL.match(regex)) {
+        alert("Successful match");
+    } else {
+        alert("No match");
+    }
+}
+
